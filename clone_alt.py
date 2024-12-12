034ba48fadcdcc5ea3853faeb58c6dfdfdbdb424
Neo <neoforevershog@gmail.com>
2024-12-12 22:27:44 +0700

    Update clone_alt.py

--- clone_alt.py
+++ clone_alt.py
@@ -99,8 +99,7 @@ def run_script():
             print("No changes were needed.")
 
     # Define the repo path
-    repo_path = '/content/Retrieval-based-Voice-Conversion-WebUI'
-
+    repo_path = '/content/rvc"
     def copy_all_files_in_directory(src_dir, dest_dir):
         # Iterate over all files in source directory
         for item in Path(src_dir).glob('*'):
@@ -147,20 +146,20 @@ def run_script():
     clone_and_copy_repo(repo_path)
 
     # Download the credentials file for RVC archive sheet
-    os.makedirs('/content/Retrieval-based-Voice-Conversion-WebUI/stats/', exist_ok=True)
+    os.makedirs('/content/rvc/stats/', exist_ok=True)
     run_cmd("wget -q https://cdn.discordapp.com/attachments/945486970883285045/1114717554481569802/peppy-generator-388800-07722f17a188.json -O /content/Retrieval-based-Voice-Conversion-WebUI/stats/peppy-generator-388800-07722f17a188.json")
 
     # Forcefully delete any existing torchcrepe dependencies downloaded from an earlier run just in case
-    shutil.rmtree('/content/Retrieval-based-Voice-Conversion-WebUI/torchcrepe', ignore_errors=True)
+    shutil.rmtree('/content/rvc/torchcrepe', ignore_errors=True)
     shutil.rmtree('/content/torchcrepe', ignore_errors=True)
 
     # Download the torchcrepe folder from the maxrmorrison/torchcrepe repository
     run_cmd("git clone https://github.com/maxrmorrison/torchcrepe.git")
-    shutil.move('/content/torchcrepe/torchcrepe', '/content/Retrieval-based-Voice-Conversion-WebUI/')
+    shutil.move('/content/torchcrepe/torchcrepe', '/content/rvc/')
     shutil.rmtree('/content/torchcrepe', ignore_errors=True)  # Delete the torchcrepe repository folder
 
     # Change the current directory to /content/Retrieval-based-Voice-Conversion-WebUI
-    os.chdir('/content/Retrieval-based-Voice-Conversion-WebUI')
+    os.chdir('/content/rvc')
     os.makedirs('pretrained', exist_ok=True)
     os.makedirs('uvr5_weights', exist_ok=True)
 
@@ -196,7 +195,7 @@ def download_pretrained_models():
     }
 
     base_url = "https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/"
-    base_path = "/content/Retrieval-based-Voice-Conversion-WebUI/"
+    base_path = "/content/rvc/"
 
     # Calculate total number of files to download
     total_files = sum(len(files) for files in pretrained_models.values()) + 1  # +1 for hubert_base.pt
